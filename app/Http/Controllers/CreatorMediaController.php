ea7a5018c54e18d35444b6a0b8eec19e913d493b
Iker <21isanchez@gmail.com>
2025-03-05 22:20:27 +0100

    #

---
+++ app/Http/Controllers/CreatorMediaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CreatorMedia;
+use App\Http\Requests\StoreCreatorMediaRequest;
+use App\Http\Requests\UpdateCreatorMediaRequest;
+
+class CreatorMediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCreatorMediaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CreatorMedia $creatorMedia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CreatorMedia $creatorMedia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCreatorMediaRequest $request, CreatorMedia $creatorMedia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CreatorMedia $creatorMedia)
+    {
+        //
+    }
+}
---
+++ app/Models/Media.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    /** @use HasFactory<\Database\Factories\MediaFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'release_year',
+        'type',
+        'category_id',
+    ];
+
+    // Relations
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function collections() {
+        return $this->belongsToMany(Collection::class, 'collection_media')->withTimestamps();
+    }
+
+    public function reviews() {
+        return $this->hasMany(Review::class);
+    }
+
+    public function watchlist() {
+        return $this->belongsToMany(User::class, 'watchlist')->withTimestamps();
+    }
+
+    public function platforms() {
+        return $this->belongsToMany(Platform::class, 'media_platform')->withTimestamps();
+    }
+
+    public function creators() {
+        return $this->belongsToMany(Creator::class, 'creator_media')->withTimestamps();
+    }
+
+}
