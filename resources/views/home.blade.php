5453cdb62ad27c4d80019c69dcad57b92ba030fb
Iker <21isanchez@gmail.com>
2025-04-01 16:11:33 +0200

    #

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,6 @@
-<!-- component -->
-<link rel="preconnect" href="https://rsms.me/">
-<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-<style>
-    :root {
-        font-family: 'Inter', sans-serif;
-    }
+@extends('layout')
 
-    @supports (font-variation-settings: normal) {
-        :root {
-            font-family: 'Inter var', sans-serif;
-        }
-    }
-</style>
-
-<div class="antialiased bg-black w-full min-h-screen text-slate-300 relative py-4">
-    <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
-        <x-menu>
-        </x-menu>
-
-        <x-content>
-            <x-inicio>
-            </x-inicio>
-        </x-content>
-    </div>
-</div>
+@section('content')
+    <x-inicio>
+    </x-inicio>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MediaHUB</title>
+    <!-- component -->
+    <link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <style>
+        :root {
+            font-family: 'Inter', sans-serif;
+        }
+
+        @supports (font-variation-settings: normal) {
+            :root {
+                font-family: 'Inter var', sans-serif;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="antialiased bg-black w-full min-h-screen text-slate-300 relative py-4">
+        <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
+            <x-menu>
+            </x-menu>
+            <x-content>
+                @yield('content')
+            </x-content>
+        </div>
+    </div>
+</body>
+
+</html>
